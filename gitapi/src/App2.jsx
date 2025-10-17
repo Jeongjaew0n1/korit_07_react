f7d840baf822e674813b48e98f157a68f310a597
Jeongjaew0n1 <qlgmq12@gmail.com>
2025-10-17 16:45:54 +0900

    feat.20251017

--- gitapi/src/App2.jsx
+++ gitapi/src/App2.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import Repositories from './Repositories'
 
 const queryClient = new QueryClient();
 
@@ -9,7 +8,7 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <Repositories />
+
       </QueryClientProvider>
     </>
   )
